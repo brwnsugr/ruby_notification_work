4e481328b2d2d9a97ebfa41ea099f289c83434ef
eunho <eunho.lee@stunitas.com>
2020-06-13 17:57:03 +0900

    1st commit/basic Design and Implementation

---
+++ controllers/community_notifications_controller.rb
@@ -0,0 +1,27 @@
+require_relative '../models/community_push_notification'
+class CommunityNotificationsController ## < ApplicationController
+  PERMITTED_KEYS = [
+    'user_id', 'notification_type_id', 'sender_id', 'sender_type', 'target_id', 'target_type', 'created_at' 
+  ]
+
+  def get_notifications_for_user(notifications, user_id)
+    # if user_id = 'all' -> Get notifications for all users
+    parsed_notifications = JSON.parse(notifications)
+    filtered_notifications_by_user = user_id.eql?('all') ? parsed_notifications : parsed_notifications.select{ |item| item['user_id'].eql?(user_id) }
+    permit_notification_key_params(filtered_notifications_by_user)
+    
+    raise "User_id was not Found or Notification file is empty. Please Check Again!" if filtered_notifications_by_user.empty? 
+
+    CommunityPushNotification.build_merged_notifications(filtered_notifications_by_user)
+  end
+
+private
+  def permit_notification_key_params(notifications)
+    notifications.each do |item|
+      item.keys.each{|key| item.delete(key) unless PERMITTED_KEYS.include? key }
+    end
+    notifications
+  end
+
+
+end
---
+++ models/community_push_notification.rb
@@ -0,0 +1,8 @@
+class CommunityPushNotification ## < ApplicationRecord
+
+  def self.build_merged_notifications(notifications)
+    puts "here's model class method!"
+    
+  end
+
+end
