7e27a12f3d3d78320e91276572d36e75c9882051
swdream <swdream.a@gmail.com>
2020-06-14 23:18:34 +0900

    test cases added(1)

--- test/test_case.rb
+++ test/test_case.rb
@@ -276,6 +276,378 @@ module TestCase
           }
         ]
       }
+    },
+
+    case7: {
+      output: [
+        '[1970-01-01 08:00:10] seedly_user1 commented on a question',
+        '[1970-01-01 08:16:39] seedly_user4 upvoted a question',
+        '[1970-01-01 08:16:39] seedly_user4 commented on a question',
+        '[1970-01-27 01:17:02] seedly_user44 answered a question',
+        '[1970-03-06 15:12:02] seedly_user999 and seedly_user4 answered a question'
+      ],
+      input:{
+        user_id: 'seedly_user2',
+        notifications: [
+          {
+            "user_id": "seedly_user2",
+            "notification_type_id": 2,
+            "sender_id": "seedly_user1",
+            "sender_type": "User",
+            "target_id": 1,
+            "target_type": "Question",
+            "created_at": 10000
+          },
+          {
+            "user_id": "seedly_user2",
+            "notification_type_id": 1,
+            "sender_id": "seedly_user999",
+            "sender_type": "User",
+            "target_id": 22,
+            "target_type": "Question",
+            "created_at": 5555522222
+          },
+          {
+            "user_id": "seedly_user1",
+            "notification_type_id": 1,
+            "sender_id": "seedly_user99",
+            "sender_type": "User",
+            "target_id": 22,
+            "target_type": "Question",
+            "created_at": 2222222222
+          },
+          {
+            "user_id": "seedly_user2",
+            "notification_type_id": 1,
+            "sender_id": "seedly_user44",
+            "sender_type": "User",
+            "target_id": 55,
+            "target_type": "Question",
+            "created_at": 2222222223
+          },
+          {
+            "user_id": "seedly_user2",
+            "notification_type_id": 1,
+            "sender_id": "seedly_user4",
+            "sender_type": "User",
+            "target_id": 22,
+            "target_type": "Question",
+            "created_at": 2222222221
+          },
+          {
+            "user_id": "seedly_user2",
+            "notification_type_id": 3,
+            "sender_id": "seedly_user4",
+            "sender_type": "User",
+            "target_id": 22,
+            "target_type": "Question",
+            "created_at": 999998
+          },
+          {
+            "user_id": "seedly_user2",
+            "notification_type_id": 2,
+            "sender_id": "seedly_user4",
+            "sender_type": "User",
+            "target_id": 22,
+            "target_type": "Question",
+            "created_at": 999999
+          }
+        ]
+      }
+    },
+    
+    case8: {
+      output: [
+        '[1970-03-06 15:12:02] seedly_user999 upvoted a question',
+      ],
+      input:{
+        user_id: 'seedly_user2',
+        notifications: [
+          {
+            "user_id": "seedly_user2",
+            "notification_type_id": 5,
+            "sender_id": "seedly_user1",
+            "sender_type": "User",
+            "target_id": 1,
+            "target_type": "Question",
+            "created_at": 10000
+          },
+          {
+            "user_id": "seedly_user2",
+            "notification_type_id": 3,
+            "sender_id": "seedly_user999",
+            "sender_type": "User",
+            "target_id": 22,
+            "target_type": "Question",
+            "created_at": 5555522222
+          },
+          {
+            "user_id": "seedly_user1",
+            "notification_type_id": 6,
+            "sender_id": "seedly_user99",
+            "sender_type": "User",
+            "target_id": 22,
+            "target_type": "Question",
+            "created_at": 2222222222
+          }
+        ]
+      }
+    },
+
+    case9: {
+      output: [
+        '[1970-03-06 15:12:02] seedly_user999 upvoted a question',
+      ],
+      input:{
+        user_id: 'seedly_user2',
+        notifications: [
+          {
+            "user_id": "seedly_user2",
+            "notification_type_id": 5,
+            "sender_id": "seedly_user1",
+            "sender_type": "User",
+            "target_id": 1,
+            "target_type": "Question",
+            "created_at": 10000
+          },
+          {
+            "user_id": "seedly_user2",
+            "notification_type_id": 3,
+            "sender_id": "seedly_user999",
+            "sender_type": "User",
+            "target_id": 22,
+            "target_type": "Question",
+            "created_at": 5555522222
+          },
+          {
+            "user_id": "seedly_user1",
+            "notification_type_id": 6,
+            "sender_id": "seedly_user99",
+            "sender_type": "User",
+            "target_id": 22,
+            "target_type": "Question",
+            "created_at": 2222222222
+          }
+        ]
+      }
+    },
+
+    case10: {
+      output: [
+        '[1970-01-01 08:01:40] seedly_user99 answered a question',
+        '[1970-01-12 21:46:39] seedly_user1 commented on a question',
+        '[1970-03-06 15:12:02] seedly_user999 upvoted a question'
+      ],
+      input:{
+        user_id: 'test_user2',
+        notifications: [
+          {
+            "user_id": "test_user2",
+            "notification_type_id": 2,
+            "sender_id": "seedly_user1",
+            "sender_type": "User",
+            "target_id": 100,
+            "target_type": "Question",
+            "created_at": 999999999
+          },
+          {
+            "user_id": "test_user2",
+            "notification_type_id": 3,
+            "sender_id": "seedly_user999",
+            "sender_type": "User",
+            "target_id": 100,
+            "target_type": "Question",
+            "created_at": 5555522222
+          },
+          {
+            "user_id": "test_user2",
+            "notification_type_id": 1,
+            "sender_id": "seedly_user99",
+            "sender_type": "User",
+            "target_id": 100,
+            "target_type": "Question",
+            "created_at": 100000
+          }
+        ]
+      }
+    },
+
+    case11: {
+      output: [
+        '[2019-11-22 10:48:13] funeralpierce upvoted a question',
+        '[2019-11-22 10:48:21] hackamorevisiting upvoted a question',
+        '[2019-11-22 10:48:22] jellybyunmarked upvoted a question',
+        '[2019-11-22 10:48:25] funeralpierce and backwarddusty upvoted a question'
+      ],
+      input:{
+        user_id: 'windboundeast',
+        notifications: [
+          {
+            "user_id": "windboundeast",
+            "notification_type_id": 3,
+            "sender_id": "jellybyunmarked",
+            "sender_type": "User",
+            "target_id": 901,
+            "target_type": "Question",
+            "created_at": 1574390902542
+          },
+          {
+            "user_id": "windboundeast",
+            "notification_type_id": 3,
+            "sender_id": "funeralpierce",
+            "sender_type": "User",
+            "target_id": 201,
+            "target_type": "Question",
+            "created_at": 1574390893055
+          },
+          {
+            "user_id": "windboundeast",
+            "notification_type_id": 3,
+            "sender_id": "hackamorevisiting",
+            "sender_type": "User",
+            "target_id": 101,
+            "target_type": "Question",
+            "created_at": 1574390901458
+          },
+          {
+            "user_id": "windboundeast",
+            "notification_type_id": 3,
+            "sender_id": "backwarddusty",
+            "sender_type": "User",
+            "target_id": 401,
+            "target_type": "Question",
+            "created_at": 1574390888477
+          },
+          {
+            "user_id": "windboundeast",
+            "notification_type_id": 3,
+            "sender_id": "funeralpierce",
+            "sender_type": "User",
+            "target_id": 401,
+            "target_type": "Question",
+            "created_at": 1574390905519
+          }
+        ]
+      }
+    },
+
+    case12: {
+      output: [
+        '[2019-11-22 10:48:13] funeralpierce upvoted a question',
+        '[2019-11-22 10:48:21] funeralpierce and backwarddusty upvoted a question',
+        '[2019-11-22 10:48:21] hackamorevisiting upvoted a question',
+        '[2019-11-22 10:48:22] jellybyunmarked upvoted a question',
+      ],
+      input:{
+        user_id: 'windboundeast',
+        notifications: [
+          {
+            "user_id": "windboundeast",
+            "notification_type_id": 3,
+            "sender_id": "jellybyunmarked",
+            "sender_type": "User",
+            "target_id": 901,
+            "target_type": "Question",
+            "created_at": 1574390902542
+          },
+          {
+            "user_id": "windboundeast",
+            "notification_type_id": 3,
+            "sender_id": "funeralpierce",
+            "sender_type": "User",
+            "target_id": 201,
+            "target_type": "Question",
+            "created_at": 1574390893055
+          },
+          {
+            "user_id": "windboundeast",
+            "notification_type_id": 3,
+            "sender_id": "hackamorevisiting",
+            "sender_type": "User",
+            "target_id": 101,
+            "target_type": "Question",
+            "created_at": 1574390901458
+          },
+          {
+            "user_id": "windboundeast",
+            "notification_type_id": 3,
+            "sender_id": "backwarddusty",
+            "sender_type": "User",
+            "target_id": 401,
+            "target_type": "Question",
+            "created_at": 1574390888477
+          },
+          {
+            "user_id": "windboundeast",
+            "notification_type_id": 3,
+            "sender_id": "funeralpierce",
+            "sender_type": "User",
+            "target_id": 401,
+            "target_type": "Question",
+            "created_at": 1574390901457
+          }
+        ]
+      }
+    },
+
+    case13: {
+      output: [
+        '[2019-11-22 10:48:08] backwarddusty upvoted a question',
+        '[2019-11-22 10:48:13] funeralpierce upvoted a question',
+        '[2019-11-22 10:48:21] funeralpierce upvoted a question',
+        '[2019-11-22 10:48:21] hackamorevisiting upvoted a question',
+        '[2019-11-22 10:48:22] jellybyunmarked upvoted a question',
+      ],
+      input:{
+        user_id: 'windboundeast',
+        notifications: [
+          {
+            "user_id": "windboundeast",
+            "notification_type_id": 3,
+            "sender_id": "jellybyunmarked",
+            "sender_type": "User",
+            "target_id": 901,
+            "target_type": "Question",
+            "created_at": 1574390902542
+          },
+          {
+            "user_id": "windboundeast",
+            "notification_type_id": 3,
+            "sender_id": "funeralpierce",
+            "sender_type": "User",
+            "target_id": 201,
+            "target_type": "Question",
+            "created_at": 1574390893055
+          },
+          {
+            "user_id": "windboundeast",
+            "notification_type_id": 3,
+            "sender_id": "hackamorevisiting",
+            "sender_type": "User",
+            "target_id": 101,
+            "target_type": "Question",
+            "created_at": 1574390901458
+          },
+          {
+            "user_id": "windboundeast",
+            "notification_type_id": 3,
+            "sender_id": "backwarddusty",
+            "sender_type": "User",
+            "target_id": 401,
+            "target_type": "Question",
+            "created_at": 1574390888477
+          },
+          {
+            "user_id": "windboundeast",
+            "notification_type_id": 3,
+            "sender_id": "funeralpierce",
+            "sender_type": "User",
+            "target_id": 402,
+            "target_type": "Question",
+            "created_at": 1574390901457
+          }
+        ]
+      }
     }
 
   }
